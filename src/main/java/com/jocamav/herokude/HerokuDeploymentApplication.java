c8156ca132e4e5786bdc5f9cea3a6fd4335f3264
MARTINEZ VEGA Jose Carlos <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-11-14 12:26:50 +0100

    Changing controller

--- src/main/java/com/jocamav/herokude/HerokuDeploymentApplication.java
+++ src/main/java/com/jocamav/herokude/HerokuDeploymentApplication.java
@@ -11,7 +11,7 @@ public class HerokuDeploymentApplication {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello World!";
+        return "Hello World!!!";
     }
 
     public static void main(String[] args) {
